73db3ec3ff48a9d52840b152a0d8bed3d77814b1
Jayneel Mahival <jm.mahival@gmail.com>
2024-08-21 20:18:33 +0530

    made startergame class functional

--- src/StartGame.java
+++ src/StartGame.java
@@ -1,38 +1,29 @@
-
 import java.util.Scanner;
 
 public class StartGame {
-    
-    void Start(){
-        String [] PlayerPokemon = new String[6];
-        Scanner scanChoice = new Scanner(System.in);
-        CreatePokemon Starters = new CreatePokemon();
-        String StarterChoice;
-        System.out.println("HI trainer let us begin your journey!");
-        System.out.println("Pick a starter pokemon");
-        System.out.println(Starters.Pokemon[0].Name);
-        System.out.println(Starters.Pokemon[1].Name);
-        System.out.println(Starters.Pokemon[2].Name);
-        System.out.println("Enter the name of the pokemon you want");
 
+    String StarterPokemon() {
+        String PlayerPokemon = "tatti";
+        try (Scanner scanChoice = new Scanner(System.in)) {
+            PokemonArray Starters = new PokemonArray();
+            String StarterChoice;
+            System.out.println("Hi! trainer let us begin your journey!");
+            System.out.println("Pick a starter pokemon : ");
+            System.out.println(Starters.pokemons[0].getName());
+            System.out.println(Starters.pokemons[1].getName());
+            System.out.println(Starters.pokemons[2].getName());
+            System.out.print("Enter the name of the pokemon you want : ");
 
-        StarterChoice = scanChoice.nextLine();
+            StarterChoice = scanChoice.nextLine();
 
-        if(StarterChoice.equals(Starters.Pokemon[0].Name)){
-            System.out.println("You picked Bulbasaur");
-            PlayerPokemon[0] = Starters.Pokemon[0].Name;
+            if (StarterChoice.equals(Starters.pokemons[0].getName())) {
+                PlayerPokemon = Starters.pokemons[0].getName();
+            } else if (StarterChoice.equals(Starters.pokemons[1].getName())) {
+                PlayerPokemon = Starters.pokemons[1].getName();
+            } else if (StarterChoice.equals(Starters.pokemons[2].getName())) {
+                PlayerPokemon = Starters.pokemons[2].getName();
+            }
         }
-        else if(StarterChoice.equals(Starters.Pokemon[1].Name)){
-            System.out.println("You picked Charmander");
-            PlayerPokemon[0] = Starters.Pokemon[1].Name;
-        }
-        else if(StarterChoice.equals(Starters.Pokemon[2].Name)){
-            System.out.println("You picked Squirtle");
-            PlayerPokemon[0] = Starters.Pokemon[2].Name;
-        }
-        
-
-        scanChoice.close();
-
+        return PlayerPokemon;
     }
 }
