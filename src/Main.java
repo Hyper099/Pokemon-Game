cf1f84dd9b828dace5cc4bc1c7aa7553da75fa48
Jayneel Mahival <jm.mahival@gmail.com>
2024-08-22 22:24:13 +0530

    removed userchoice kyuki sir dukh gaya :)

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ public class Main {
    public static void main(String[] args) {
       StartGame game = new StartGame();
       PokemonStats selectedPokemon = game.selectStarterPokemon();
-      UserChoice userChoice = new UserChoice();
 
       System.out.println("\nLet's start your adventure with " + selectedPokemon.getName() + "!\n");
-      userChoice.userChoice(selectedPokemon);
-      userChoice.closeScanner();
    }
 }
