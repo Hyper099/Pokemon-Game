4093aad0f2a521c6a36146972018834f10a1ba21
Jayneel Mahival <jm.mahival@gmail.com>
2024-08-24 00:49:13 +0530

    made fight.java interactive by taking users input

--- src/Fight.java
+++ src/Fight.java
@@ -2,51 +2,86 @@ import java.util.Random;
 import java.util.Scanner;
 
 import PokemonData.PokemonStats;
+import PokemonMoves.Move;
 
 public class Fight {
-    private int damage;
-    private final PokemonStats selectedPokemon;
-    private final PokemonStats computerPokemon;
+    private int damage = 0; // Variable to hold the damage value for attacks
+    private final PokemonStats selectedPokemon; // The player's selected Pokémon
+    private final PokemonStats computerPokemon; // The computer's Pokémon
 
+    // Constructor to initialize the selected and computer Pokémon
     public Fight(PokemonStats selectedPokemon, PokemonStats computerPokemon) {
         this.selectedPokemon = selectedPokemon;
         this.computerPokemon = computerPokemon;
     }
 
-    public void performAction() {
-        System.out.println(selectedPokemon.getAttack());
-        System.out.println(computerPokemon.getSpeed());
-    }
-
+    // Method to fight
     public void fight() {
-        Scanner scanMove = new Scanner(System.in);
-        Random compMove = new Random();
 
-        // Storing HP of playerPokemon and computerPokemon
+        Scanner scanMove = new Scanner(System.in); // Scanner to read user input for moves
+        Random compMove = new Random(); // Random object for generating random moves for the computer
+
+        // Storing HP of player Pokémon and computer Pokémon
         int playerPokemonHP = selectedPokemon.getHP();
         int computerPokemonHP = computerPokemon.getHP();
 
-        boolean isPlaying = true;
-        int selectedMove = 0;
-        int userMovePower = 0;
-        int computerMovePower;
+        int playerPokemonAttack = selectedPokemon.getAttack();
+        int computerPokemonAttack = computerPokemon.getAttack();
 
+        int userMovePower; // Variable to hold the power of the user's move
+        int computerMovePower; // Variable to hold the power of the computer's move
+
+        System.out.println("\n" + selectedPokemon.getName() + " V/S " + computerPokemon.getName() + "\n");
+
+        boolean isPlaying = true;
         while (isPlaying) {
             System.out.println("Select a move to attack: ");
-            System.out.println(selectedPokemon.getMoves().getMove(0));
-            System.out.println(selectedPokemon.getMoves().getMove(1));
-            System.out.println(selectedPokemon.getMoves().getMove(2));
-            System.out.println(selectedPokemon.getMoves().getMove(3));
 
-            selectedMove = scanMove.nextInt();
+            for (int i = 0; i < selectedPokemon.getMoves().getSize(); i++) {
+                Move move = selectedPokemon.getMoves().getMove(i);
+                System.out.print((i + 1) + ". " + move.getName() + " ");
+            }
 
+            System.out.println();
+            int selectedMove = scanMove.nextInt();
+            selectedMove -= 1;
+
+            // Determine the power of the selected move based on user input
             switch (selectedMove) {
-                case 1 -> userMovePower = selectedPokemon.getMoves().getMove(0).getPower();
-                case 2 -> userMovePower = selectedPokemon.getMoves().getMove(1).getPower();
-                case 3 -> userMovePower = selectedPokemon.getMoves().getMove(2).getPower();
-                case 4 -> userMovePower = selectedPokemon.getMoves().getMove(3).getPower();
-                default -> System.out.println("Invalid choice");
+                case 0 -> userMovePower = selectedPokemon.getMoves().getMove(0).getPower();
+                case 1 -> userMovePower = selectedPokemon.getMoves().getMove(1).getPower();
+                case 2 -> userMovePower = selectedPokemon.getMoves().getMove(2).getPower();
+                case 3 -> userMovePower = selectedPokemon.getMoves().getMove(3).getPower();
+                default -> {
+                    System.out.println("Invalid choice");
+                    continue;
+                }
+            }
+            System.out.println(userMovePower);
+
+            // Additional fight logic (like attacking and checking HP) would go here
+
+            damage = (userMovePower + playerPokemonAttack) / 2; // yeh logic google pe dekha bohot bada logic hai iska.
+
+            computerPokemonHP -= damage;
+            System.out.println(selectedPokemon.getName() + " attacks " + computerPokemon.getName() + " with "
+                    + selectedPokemon.getMoves().getMove(selectedMove).getName() + " for " + damage
+                    + " damage!");
+
+            if (computerPokemonHP <= 0) {
+                System.out.println(computerPokemon.getName() + "'s remaining HP: " + 0);
+
+            } else {
+                System.out.println(computerPokemon.getName() + "'s remaining HP: " + computerPokemonHP);
+
+            }
+
+            if (computerPokemonHP <= 0) {
+                System.out.println(computerPokemon.getName() + " has fainted! You win!");
+                isPlaying = false;
+                // break;
             }
         }
+        scanMove.close();
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,6 @@
-import java.util.Scanner;
-
 import PokemonData.PokemonStats;
 import PokemonMoves.Move;
+import java.util.Scanner;
 
 public class Menu {
 
@@ -23,42 +22,44 @@ public class Menu {
 
    public void menuActions(PokemonStats selectedPokemon, PokemonStats computerPokemon) {
 
-      boolean continuePlaying = true;
-      while (continuePlaying) {
+      displayMenu();
 
-         displayMenu();
+      System.out.print("Select your action (select index): ");
 
-         System.out.print("Select your action (select index): ");
-
-         switch (getChoice()) {
-            case 1 -> {
-               System.out.println("Commencing fight.");
-               Fight fight = new Fight(selectedPokemon, computerPokemon);
-               fight.performAction();
-            }
-            case 2 -> {
-               System.out.println("\nYou chose to display Pokémon info\n");
-               System.out.println("Displaying selected Pokémon info:");
-               System.out.println("Name: " + selectedPokemon.getName());
-               System.out.println("Type: " + selectedPokemon.getType());
-               System.out.println("HP: " + selectedPokemon.getHP());
-               System.out.println("Attack: " + selectedPokemon.getAttack());
-               System.out.println("Defense: " + selectedPokemon.getDefense());
-               System.out.println("Speed: " + selectedPokemon.getSpeed());
-               System.out.println("Moves:");
-
-               for (int i = 0; i < selectedPokemon.getMoves().getSize(); i++) {
-                  Move move = selectedPokemon.getMoves().getMove(i);
-                  System.out.println((i + 1) + ": " + move.getName() + " (Power: " + move.getPower() + ")");
-               }
-            }
-            case 3 -> {
-               System.out.println("Exiting the game.");
-               scanner.close();
-               continuePlaying = false;
-            }
-            default -> System.out.println("Invalid choice! Try again.");
+      switch (getChoice()) {
+         case 1 -> {
+            System.out.println("Commencing fight.");
+            Fight fight = new Fight(selectedPokemon, computerPokemon); 
+            fight.fight();
          }
+         case 2 -> {
+            System.out.println("\nYou chose to display Pokémon info\n");
+            System.out.println("Displaying selected Pokémon info:");
+            System.out.println("Name: " + selectedPokemon.getName());
+            System.out.println("Type: " + selectedPokemon.getType());
+            System.out.println("HP: " + selectedPokemon.getHP());
+            System.out.println("Attack: " + selectedPokemon.getAttack());
+            System.out.println("Defense: " + selectedPokemon.getDefense());
+            System.out.println("Speed: " + selectedPokemon.getSpeed());
+            System.out.println("Moves:");
+
+            for (int i = 0; i < selectedPokemon.getMoves().getSize(); i++) {
+               Move move = selectedPokemon.getMoves().getMove(i);
+               System.out.println((i + 1) + ": " + move.getName() + " (Power: " + move.getPower() + ")");
+            }
+            menuActions(selectedPokemon, computerPokemon);
+
+         }
+         case 3 -> {
+            System.out.println("Exiting the game.");
+            scanner.close();
+
+         }
+         default -> {
+            System.out.println("Invalid choice! Try again.");
+            menuActions(selectedPokemon, computerPokemon);
+         }
+
       }
    }
 }
