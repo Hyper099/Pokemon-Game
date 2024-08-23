298ac5a674a5c647973f337feab6d17effa12d1f
Jayneel Mahival <jm.mahival@gmail.com>
2024-08-23 19:12:55 +0530

    updates PokemonArray

--- src/PokemonArray.java
+++ src/PokemonArray.java
@@ -4,22 +4,44 @@ import PokemonData.Squirtle;
 import PokemonMoves.Moves;
 
 class PokemonArray {
-   public PokemonStats[] pokemons = new PokemonStats[3];
+      public PokemonStats[] pokemons = new PokemonStats[3];
 
-   public PokemonArray() {
-      // CALL OBJECTS OF POKEMON
-      Charmander charmander = new Charmander();
-      Bulbasaur bulbasaur = new Bulbasaur();
-      Squirtle squirtle = new Squirtle();
+      public PokemonArray() {
+            // CALL OBJECTS OF POKEMON
+            Charmander charmander = new Charmander();
+            Bulbasaur bulbasaur = new Bulbasaur();
+            Squirtle squirtle = new Squirtle();
 
-      // ASSIGN MOVES TO THE POKEMON OBJETS
-      Moves charmanderMoves = charmander.getMoves();
-      Moves bulbasaurMoves = bulbasaur.getMoves();
-      Moves squirtleMoves = squirtle.getMoves();
+            // ASSIGN MOVES TO THE POKEMON OBJETS
+            Moves charmanderMoves = charmander.getMoves();
+            Moves bulbasaurMoves = bulbasaur.getMoves();
+            Moves squirtleMoves = squirtle.getMoves();
 
-      pokemons[0] = new PokemonStats("Charmander", "fire", 110, 20, 15, 40, charmanderMoves);
-      pokemons[1] = new PokemonStats("Bulbasaur", "grass", 120, 10, 30, 35, bulbasaurMoves);
-      pokemons[2] = new PokemonStats("Squirtle", "water", 150, 15, 5, 30,
-            squirtleMoves);
-   }
+            pokemons[0] = new PokemonStats(
+                        charmander.getName(),
+                        charmander.getType(),
+                        charmander.getHP(),
+                        charmander.getAttack(),
+                        charmander.getDefense(),
+                        charmander.getSpeed(),
+                        charmanderMoves);
+
+            pokemons[1] = new PokemonStats(
+                        bulbasaur.getName(),
+                        bulbasaur.getType(),
+                        bulbasaur.getHP(),
+                        bulbasaur.getAttack(),
+                        bulbasaur.getDefense(),
+                        bulbasaur.getSpeed(),
+                        bulbasaurMoves);
+
+            pokemons[2] = new PokemonStats(
+                        squirtle.getName(),
+                        squirtle.getType(),
+                        squirtle.getHP(),
+                        squirtle.getAttack(),
+                        squirtle.getDefense(),
+                        squirtle.getSpeed(),
+                        squirtleMoves);
+      }
 }
--- src/PokemonData/Bulbasaur.java
+++ src/PokemonData/Bulbasaur.java
@@ -4,12 +4,12 @@ import PokemonMoves.GrassMoves;
 import PokemonMoves.Moves;
 
 public class Bulbasaur {
-    public String Name = "Bulbasaur";
+    public String name = "Bulbasaur";
     public String type = "Grass";
-    public int HP = 120;
-    public int Attack = 20;
-    public int Defence = 30;
-    public int Speed = 35;
+    public int hp = 120;
+    public int attack = 20;
+    public int defense = 30;
+    public int speed = 35;
     public Moves Moves;
 
     public Bulbasaur() {
@@ -18,6 +18,30 @@ public class Bulbasaur {
                 GrassMoves.Tackle,
                 GrassMoves.Leech_seed,
                 GrassMoves.Growl);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getHP() {
+        return hp;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getSpeed() {
+        return speed;
     }
 
     public Moves getMoves() {
