5d836e6b8a6b2d78756c2cb65edb81e7ad362624
Jayneel Mahival <jm.mahival@gmail.com>
2024-08-24 16:07:07 +0530

    corrected figth logic a bit and shortened the code

--- src/Fight.java
+++ src/Fight.java
@@ -5,9 +5,10 @@ import PokemonData.PokemonStats;
 import PokemonMoves.Move;
 
 public class Fight {
-    private int damage = 0; // Variable to hold the damage value for attacks
     private final PokemonStats selectedPokemon; // The player's selected Pokémon
     private final PokemonStats computerPokemon; // The computer's Pokémon
+    private final Scanner scanMove = new Scanner(System.in); // Scanner to read user input for moves
+    private final Random compMove = new Random(); // Random object for generating random moves for the computer
 
     // Constructor to initialize the selected and computer Pokémon
     public Fight(PokemonStats selectedPokemon, PokemonStats computerPokemon) {
@@ -17,179 +18,77 @@ public class Fight {
 
     // Method to fight
     public void fight() {
-
-        Scanner scanMove = new Scanner(System.in); // Scanner to read user input for moves
-        Random compMove = new Random(); // Random object for generating random moves for the computer
-
-        // Storing HP of player Pokémon and computer Pokémon
         int playerPokemonHP = selectedPokemon.getHP();
         int computerPokemonHP = computerPokemon.getHP();
-        //Attack stats of mons
-        int playerPokemonAttack = selectedPokemon.getAttack();
-        int computerPokemonAttack = computerPokemon.getAttack();
-
-        //Defence stats of mons
-
-        int playerPokemonDefence = selectedPokemon.getDefense();
-        int computerPokemonDefence = computerPokemon.getDefense();
-
-        //Speed stats of mons
-
-        int playerPokemonSpeed = selectedPokemon.getSpeed();
-        int computerPokemonSpeed = computerPokemon.getSpeed();
-
-        int userMovePower; // Variable to hold the power of the user's move
-        int computerMoveIndex;
-        String computerMove;
-        int computerMovePower; // Variable to hold the power of the computer's move
 
         System.out.println("\n" + selectedPokemon.getName() + " V/S " + computerPokemon.getName() + "\n");
 
-        boolean isPlaying = true;
-        while (isPlaying) {
-            System.out.println("Select a move to attack:");
+        while (true) {
+            int userMovePower = getUserMovePower();
+            int computerMoveIndex = compMove.nextInt(computerPokemon.getMoves().getSize());
+            int computerMovePower = computerPokemon.getMoves().getMove(computerMoveIndex).getPower();
 
-            for (int i = 0; i < selectedPokemon.getMoves().getSize(); i++) {
-                Move move = selectedPokemon.getMoves().getMove(i);
-                System.out.print((i + 1) + ". " + move.getName() + " ");
-            }
-
-            System.out.println();
-            int selectedMove = scanMove.nextInt();
-            selectedMove -= 1;
-
-            // Determine the power of the selected move based on user input
-            switch (selectedMove) {
-                case 0 -> {
-                    userMovePower = selectedPokemon.getMoves().getMove(0).getPower();
-                    
-                }
-                case 1 -> userMovePower = selectedPokemon.getMoves().getMove(1).getPower();
-                case 2 -> userMovePower = selectedPokemon.getMoves().getMove(2).getPower();
-                case 3 -> userMovePower = selectedPokemon.getMoves().getMove(3).getPower();
-                default -> {
-                    System.out.println("Invalid choice");
-                    continue;
-                }
-            }
-            // segment for randonly selecting move of computePokemon
-            computerMoveIndex = compMove.nextInt(3)+1;
-                    computerMove = computerPokemon.getMoves().getMove(computerMoveIndex).getName();
-                    computerMovePower = computerPokemon.getMoves().getMove(computerMoveIndex).getPower();
-                    
-            //System.out.println(userMovePower);
-            //System.out.println(computerMove);
-
-            //  fight logic (like attacking and checking HP) would go here, without level up feature
-
-            if(playerPokemonSpeed > computerPokemonSpeed){ //faster one attacks first
-
-                //formula for calculation damage
-
-                damage = (int)(0.5*userMovePower* (playerPokemonAttack/computerPokemonDefence))+1; //cuz 0.5 will make it a double which will be an issue
-
-                //So first player attacked first
-                System.out.println(selectedPokemon.getName() + " attacks " + computerPokemon.getName() + " with "
-                + selectedPokemon.getMoves().getMove(selectedMove).getName() + " for " + damage
-                + " damage!");
-
-                computerPokemonHP -= damage;
-
-                //Now we check if hp is 0 or not
+            if (selectedPokemon.getSpeed() > computerPokemon.getSpeed()) {
+                computerPokemonHP = executeTurn(selectedPokemon, computerPokemon, userMovePower, computerPokemonHP);
                 if (computerPokemonHP <= 0) {
                     System.out.println(computerPokemon.getName() + " has fainted! You win!");
-                    isPlaying = false;
-                    
-    
-                } else {
-                    System.out.println(computerPokemon.getName() + "'s remaining HP: " + computerPokemonHP);
-    
+                    break;
                 }
-
-
-                //Now computer's turn
-                damage = (int)(0.5*computerMovePower* (computerPokemonAttack/playerPokemonDefence))+1;
-
-                //damage window for computer
-                System.out.println(computerPokemon.getName() + " attacks " + selectedPokemon.getName() + " with "
-                + computerPokemon.getMoves().getMove(computerMoveIndex).getName() + " for " + damage
-                + " damage!");
-    
-                playerPokemonHP -= damage;
-
-                //We check if players hp is 0 or not
-                if(playerPokemonHP <=0){
-                    System.out.println(computerPokemon.getName() + " has fainted! You win!");
-                    isPlaying = false;
-    
+                playerPokemonHP = executeTurn(computerPokemon, selectedPokemon, computerMovePower, playerPokemonHP);
+                if (playerPokemonHP <= 0) {
+                    System.out.println(selectedPokemon.getName() + " has fainted! You lose!");
+                    break;
                 }
-                else {
-                    System.out.println(selectedPokemon.getName() + "'s remaining HP: " + playerPokemonHP);
+            } else {
+                playerPokemonHP = executeTurn(computerPokemon, selectedPokemon, computerMovePower, playerPokemonHP);
+                if (playerPokemonHP <= 0) {
+                    System.out.println(selectedPokemon.getName() + " has fainted! You lose!");
+                    break;
                 }
-
-            }
-
-
-
-            // if comp mon is faster , we jusr swap the attack order
-
-            else if(playerPokemonSpeed < computerPokemonSpeed){ //faster one attacks first
-
-                
-
-                //Now computer's turn
-                damage = (int)(0.5*computerMovePower* (computerPokemonAttack/playerPokemonDefence))+1;
-
-                //damage window for computer
-                System.out.println(computerPokemon.getName() + " attacks " + selectedPokemon.getName() + " with "
-                + computerPokemon.getMoves().getMove(computerMoveIndex).getName() + " for " + damage
-                + " damage!");
-
-                playerPokemonHP -= damage;
-
-                //check is player hp is 0 or not
-
-                if(playerPokemonHP <=0){
-                    System.out.println(computerPokemon.getName() + " has fainted! You win!");
-                    isPlaying = false;
-    
-                }
-                else {
-                    System.out.println(selectedPokemon.getName() + "'s remaining HP: " + playerPokemonHP);
-                }
-
-                //formula for calculation damage
-
-                damage = (int)(0.5*userMovePower* (playerPokemonAttack/computerPokemonDefence))+1; //cuz 0.5 will make it a double which will be an issue
-
-                //So first player attacked first
-                System.out.println(selectedPokemon.getName() + " attacks " + computerPokemon.getName() + " with "
-                + selectedPokemon.getMoves().getMove(selectedMove).getName() + " for " + damage
-                + " damage!");
-
-                computerPokemonHP -= damage;
-
-                //check if comp hp is 0 or not
-
+                computerPokemonHP = executeTurn(selectedPokemon, computerPokemon, userMovePower, computerPokemonHP);
                 if (computerPokemonHP <= 0) {
                     System.out.println(computerPokemon.getName() + " has fainted! You win!");
-                    isPlaying = false;
-                    
-    
-                } else {
-                    System.out.println(computerPokemon.getName() + "'s remaining HP: " + computerPokemonHP);
-    
+                    break;
                 }
-    
-
-
             }
-
-        
-          
-
-          
         }
-        scanMove.close();
+    }
+
+    // Method to get the user's move power based on their selection
+    private int getUserMovePower() {
+        System.out.println("Select a move to attack:");
+        for (int i = 0; i < selectedPokemon.getMoves().getSize(); i++) {
+            Move move = selectedPokemon.getMoves().getMove(i);
+            System.out.print((i + 1) + ". " + move.getName() + " ");
+        }
+        System.out.println();
+
+        int selectedMove = scanMove.nextInt() - 1;
+        if (selectedMove < 0 || selectedMove >= selectedPokemon.getMoves().getSize()) {
+            System.out.println("Invalid choice");
+            return getUserMovePower();
+        }
+
+        return selectedPokemon.getMoves().getMove(selectedMove).getPower();
+    }
+
+    private int executeTurn(PokemonStats attacker, PokemonStats defender, int movePower, int defenderHP) {
+        int attack = attacker.getAttack();
+        int defense = defender.getDefense();
+        int damage = (int) (0.5 * movePower * (attack / defense)) + 1;
+
+        System.out.println(attacker.getName() + " attacks " + defender.getName() + " with " +
+                attacker.getMoves().getMove(compMove.nextInt(attacker.getMoves().getSize())).getName() +
+                " for " + damage + " damage!");
+
+        defenderHP -= damage;
+
+        if (defenderHP <= 0) {
+            System.out.println(defender.getName() + "'s remaining HP: 0");
+            return 0; // Ensures HP doesn't go negative and signals fainted status
+        } else {
+            System.out.println(defender.getName() + "'s remaining HP: " + defenderHP);
+            return defenderHP;
+        }
     }
 }
--- src/PokemonMoves/Moves.java
+++ src/PokemonMoves/Moves.java
@@ -5,10 +5,15 @@ public class Moves {
 
    // pass a move like this into moves array
    public Moves(Move move1, Move move2, Move move3, Move move4) {
-      this.moves = new Move[] { move1, move2, move3, move4 };
+      this.moves = new Move[] {
+            move1,
+            move2,
+            move3,
+            move4
+      };
    }
 
-   // get a move from moves array by index like this 
+   // get a move from moves array by index like this
    public Move getMove(int index) {
       if (index >= 0 && index < moves.length) {
          return moves[index];
