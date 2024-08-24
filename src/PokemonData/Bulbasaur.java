95e928adf847e334501bb24e5025e86c9518d728
manan75 <85684429+manan75@users.noreply.github.com>
2024-08-24 18:26:00 +0530

    Update Bulbasaur.java

--- src/PokemonData/Bulbasaur.java
+++ src/PokemonData/Bulbasaur.java
@@ -6,7 +6,7 @@ import PokemonMoves.Moves;
 public class Bulbasaur extends PokemonStats {
 
     public Bulbasaur() {
-        super("Bulbasaur", "Grass", 120, 20, 30, 35, new Moves(
+        super("Bulbasaur", "Grass", 120, 20, 30, 35,5,70,20,30, new Moves(
                 GrassMoves.Vine_whip,
                 GrassMoves.Tackle,
                 GrassMoves.Leech_seed,
